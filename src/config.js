7f5fcd5caefa29f77191801bf2d40a9cc34a7e10
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2025-11-27 18:28:02 -0500

    Fixed NODE_ENV bug

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    url: !process.env.NODE_ENV ? "https://dealer-comp-api-24cd738121b2.herokuapp.com" : "http://localhost:8000"
+    url: process.env.NODE_ENV !== "development" ? "https://dealer-comp-api-24cd738121b2.herokuapp.com" : "http://localhost:8000"
 };
+
+console.log(config.url, process.env.NODE_ENV)
 
 export default config;
